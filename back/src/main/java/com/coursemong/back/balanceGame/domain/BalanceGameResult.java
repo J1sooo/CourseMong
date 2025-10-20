2815a2e09770a1f6d77945983d5b3a34b4f43999
gyuri0124 <gyul12243@naver.com>
2025-10-21 02:07:22 +0900

    feat: 밸런스 게임 관련 entity 생성

---
+++ back/src/main/java/com/coursemong/back/balanceGame/domain/BalanceGameResult.java
@@ -0,0 +1,32 @@
+package com.coursemong.back.balanceGame.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "balance_game_result")
+@Getter
+@NoArgsConstructor
+public class BalanceGameResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "balance_id", nullable = false)
+    private Long id;
+
+    @Column(name = "balance_name", length = 20, nullable = false)
+    private String balanceName;
+
+    @Column(name = "balance_content", length = 255, nullable = false)
+    private String balanceContent;
+
+    @Column(name = "character", length = 255, nullable = false)
+    private String character;
+
+    @Column(name = "good_match", length = 255, nullable = false)
+    private String goodMatch;
+
+    @Column(name = "not_good_match", length = 255, nullable = false)
+    private String notGoodMatch;
+}

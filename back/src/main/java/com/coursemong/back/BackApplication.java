fdf1746057563232ecdf072b1182fea3f05b6211
j1sooo <kjisoo1031@gmail.com>
2026-05-15 20:00:30 +0900

    refactor: TTL 스케줄러 제거
    - DateCourseScheduler.java 삭제
    - BackApplication에서 @EnableScheduling 제거
    - DateCourseRepository에서 TTL 조회 메서드 제거

--- back/src/main/java/com/coursemong/back/BackApplication.java
+++ back/src/main/java/com/coursemong/back/BackApplication.java
@@ -3,10 +3,8 @@ package com.coursemong.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableRetry
-@EnableScheduling
 @SpringBootApplication
 public class BackApplication {
 
